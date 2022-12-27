cb8ce809f88c52b81971652330bab5d2b246e54c
Cristian Venegas <cristian.venegas@materoceano.com>
2022-12-27 16:21:10 -0300

    delete: delete some files

--- controller/post.js
+++ controller/post.js
@@ -2,14 +2,13 @@ const pool = require("../config/pool");
 
 const getPost = async () => {
   const {rows} = await pool.query("SELECT * FROM post")
-  console.log(rows);
   return rows
 };
 
 const addPost = async (titulo, img, description, likes) => {
-  const query = "INSERT INTO post values (DEFAULT, $1, $2, $3, $4)";
+  const consulta = "INSERT INTO post values (DEFAULT, $1, $2, $3, $4)";
   const values = [titulo, img, description, likes];
-  const result = await pool.query(query, values);
+  const result = await pool.query(consulta, values);
   console.log("Post Agregado con exito ");
 };
 
--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-require("dotenv").config({ path: "../.env" });
-const cors = require("cors");
 const express = require("express");
 const app = express();
+
+
+const cors = require("cors");
+const { getPost, addPost } = require("./controller/post");
+require("dotenv").config({ path: "./.env" });
+
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, console.log(`servidor corriendo en el Puerto : ${PORT}`));
+app.use(express.json())
 
+app.get("/post", async (req, res) => {
+  const post = await getPost();
+  res.json(post);
+  
+})
+
+
+app.post("/post", async (req, res) => {
+  const {titulo, img, description, likes} = req.body
+  await addPost(titulo, img, description, likes)
+  res.send("Post Agregado con existo")
+})
+
+app.listen(PORT, console.log(`servidor corriendo en el Puerto : ${PORT}`));
