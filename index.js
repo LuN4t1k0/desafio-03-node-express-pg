e80bb07d9c0f0afce6e0e08d77e88cdd12589349
Cristian Venegas <cristian.venegas@materoceano.com>
2022-12-27 22:14:05 -0300

    fix : fix some problems with json

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
 const express = require("express");
 const app = express();
 
-
 const cors = require("cors");
 const { getPost, addPost } = require("./controller/post");
 require("dotenv").config({ path: "./.env" });
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-app.get("/post", async (req, res) => {
-  const post = await getPost();
-  res.json(post);
-  
-})
+/* Este es un middleware que nos permite servir archivos estáticos. */
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  try {
+    res.sendFile(__dirname + "/public/index.html");
+  } catch (error) {
+    res.json({ message: "El recurso no esta disponible " });
+  }
+});
 
-app.post("/post", async (req, res) => {
-  const {titulo, img, description, likes} = req.body
-  await addPost(titulo, img, description, likes)
-  res.send("Post Agregado con existo")
-})
+app.get("/posts", async (req, res) => {
+  try {
+    const post = await getPost();
+    let modificado = post.map((p) => ({
+      id: p.id,
+      titulo: p.titulo,
+      img: p.img,
+      descripcion: p.description,
+    }));
+    res.json(modificado);
+  } catch (error) {
+    res.json({ message: "El recurso no esta disponible " });
+  }
+});
+
+app.post("/posts", async (req, res) => {
+  try {
+    const { titulo, url, descripcion, likes } = req.body;
+    console.log(req.body);
+
+    // if (Object.values(cancion).some((value) => value === "")) {
+    //   return res.status(400).json({ message: "Falta completar un campo " });
+    // }
+
+    await addPost(titulo, url, descripcion, likes);
+    res.send("Post Agregado con existo");
+  } catch (error) {
+    res.json({ message: "El recurso no esta disponible " });
+  }
+});
 
 app.listen(PORT, console.log(`servidor corriendo en el Puerto : ${PORT}`));
